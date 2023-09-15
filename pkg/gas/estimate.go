348dc306934d620a60d4ca3ebb4c5f3b3ed0a843
0xbiubiubiu <null>
2023-09-15 16:41:23 +0800

    sync

--- pkg/gas/estimate.go
+++ pkg/gas/estimate.go
@@ -151,9 +151,9 @@ func EstimateGas(in *EstimateInput) (verificationGas uint64, callGas uint64, err
 	// expected gas cost.
 	data["maxFeePerGas"] = hexutil.EncodeBig(in.Op.MaxFeePerGas)
 	data["maxPriorityFeePerGas"] = hexutil.EncodeBig(in.Op.MaxFeePerGas)
-	vgl = big.NewInt(0).Add(vgl, big.NewInt(50000))
+	vgl = big.NewInt(0).Add(vgl, big.NewInt(20000))
 	data["verificationGasLimit"] = hexutil.EncodeBig(vgl)
-	cgl = big.NewInt(0).Add(cgl, big.NewInt(50000))
+	cgl = big.NewInt(0).Add(cgl, big.NewInt(20000))
 	data["callGasLimit"] = hexutil.EncodeBig(cgl)
 	simOp, err = userop.New(data)
 	if err != nil {
